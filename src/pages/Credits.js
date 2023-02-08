823e96a0a91d6df674664d7ebd5e10604080ebc6
BabyBlue52 <chris.hennemann256@gmail.com>
2023-02-07 21:21:09 -0500

    Reference Links

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -59,9 +59,39 @@ function CreditsPage () {
                     <h1>Credits</h1>
                     <motion.div variants={variants} animate={reveal ? 'show': 'hide'}>
                         <div className='link-set'>
+                            <p><strong>Fragmentation</strong></p>
+                            <a href="https://www.atomretro.com/product_info.cfm?product_id=16595&d=MARMALADE-RETRO-60s-MOD-FITTED-CONTRAST-TRIM-DRESS" target="_blank" rel="noreferrer">
+                                <p>https://www.atomretro.com/product_info.cfm?product_id=16595&d=MARMALADE-RETRO-60s-MOD-FITTED-CONTRAST-TRIM-DRESS</p>
+                            </a>
+                        </div>
+                        <div className='link-set'>
+                            <p><strong>Commodifcation</strong></p>
+                            <a href="https://www.fulltable.com/VTS/g/globe/gl.htm" target="_blank" rel="noreferrer">
+                                <p>https://www.fulltable.com/VTS/g/globe/gl.htm</p>
+                            </a>
+                            <a href="https://www.ebay.com/itm/155201501443?chn=ps&mkevt=1&mkcid=28" target="_blank" rel="noreferrer">
+                                <p>https://www.ebay.com/itm/155201501443?chn=ps&mkevt=1&mkcid=28</p>
+                            </a>
+                            <a href="https://www.flickr.com/photos/91591049@N00/16518783025/in/photostream/" target="_blank" rel="noreferrer">
+                                <p>https://www.flickr.com/photos/91591049@N00/16518783025/in/photostream/</p>
+                            </a>
+                            <a href="http://www.therustycar.com/the-rusty-car/vintage-ad-1967-ford-thunderbird/2014/9/23" target="_blank" rel="noreferrer">
+                                <p>http://www.therustycar.com/the-rusty-car/vintage-ad-1967-ford-thunderbird/2014/9/23</p>
+                            </a>
+                            <a href="https://www.heinzhistorycenter.org/blog/history-by-the-day-tuning-holidays-recalling-pittsburghs-connections-tv-holiday-special/" target="_blank" rel="noreferrer">
+                                <p>https://www.heinzhistorycenter.org/blog/history-by-the-day-tuning-holidays-recalling-pittsburghs-connections-tv-holiday-special/</p>
+                            </a>
+                            <a href="https://www.thehistoryoftv.com/american-tv-ads" target="_blank" rel="noreferrer">
+                                <p>https://www.thehistoryoftv.com/american-tv-ads</p>
+                            </a>
+                            <a href="https://www.pinterest.com/bluetantra/console-stereos-of-the-20th-century/" target="_blank" rel="noreferrer">
+                                <p>https://www.pinterest.com/bluetantra/console-stereos-of-the-20th-century/</p>
+                            </a>
+                        </div>
+                        <div className='link-set'>
                             <p><strong>Negation</strong></p>
                             <a href="https://www.flickr.com/photos/13476480@N07/7635043238" target="_blank" rel="noreferrer">
-                                <p><span>[1]</span>https://www.flickr.com/photos/13476480@N07/7635043238</p>
+                                <p>https://www.flickr.com/photos/13476480@N07/7635043238</p>
                             </a>
                         </div>
                         <div className='link-set'>
@@ -71,6 +101,18 @@ function CreditsPage () {
                             </a>
                             <a href="https://fineartamerica.com/featured/1960s-unhappy-sad-depressed-woman-vintage-images.html" target="_blank" rel="noreferrer">
                                 <p><span>[2]</span>https://fineartamerica.com/featured/1960s-unhappy-sad-depressed-woman-vintage-images.html</p>
+                            </a>
+                        </div>
+                        <div className='link-set'>
+                            <p><strong>Homogenization</strong></p>
+                            <a href="http://artscash.com/paintings-30.html" target="_blank" rel="noreferrer">
+                                <p>http://artscash.com/paintings-30.html</p>
+                            </a>
+                        </div>
+                        <div className='link-set'>
+                            <p><strong>Small Screen</strong></p>
+                            <a href="https://twitter.com/mubi/status/1510212125146456074" target='_blank' rel="noreferrer">
+                                <p>https://twitter.com/mubi/status/1510212125146456074</p>
                             </a>
                         </div>
                     </motion.div>
